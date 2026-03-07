e9374e2fd70abe703197c51116f4ce9f58a38d18
phillip feldman <philliplifeldman@gmail.com>
2026-03-06 16:53:46 -0800

    I want to change my method for task 2

--- src/test/java/com/jpmc/midascore/KafkaConsumer.java
+++ src/test/java/com/jpmc/midascore/KafkaConsumer.java
@@ -1,6 +1,7 @@
 package com.jpmc.midascore;
 
 import com.jpmc.midascore.foundation.Transaction;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +10,32 @@ public class KafkaConsumer {
 
 
 
-    @KafkaListener(topics = {"${general.kafka-topic}"},groupId = "com.jpmc")
-    public void consume(String quote){
-        System.out.println("I Received " + quote);
-    }
+        @KafkaListener(topics = {"${general.kafka-topic}"},groupId = "com.jpmc")
+        public void consume(String quote){
+            System.out.println("I Received " + quote);
+            String[] transactionData = quote.split(", ");
+            Transaction transaction = new Transaction(
+                            Long.parseLong(transactionData[0]),
+                            Long.parseLong(transactionData[1]),
+                            Float.parseFloat(transactionData[2])
+                    );
 
+
+        }
+
+
+
+    //The following seems to be better practice than above. Figure out how to make it work before final submission.
+    /*
+    @KafkaListener(topics = {"${general.kafka-topic}"}, groupId = "com.jpmc")
+    public void listen(ConsumerRecord<String, Transaction> record) {
+        Transaction transaction = record.value();
+        System.out.println("I Received " +transaction);
+
+
+
+    }
+    */
 
 
 }
--- src/test/java/com/jpmc/midascore/TransactionRecord.java
+++ src/test/java/com/jpmc/midascore/TransactionRecord.java
@@ -3,6 +3,9 @@ package com.jpmc.midascore;
 
 import com.jpmc.midascore.foundation.Transaction;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import org.springframework.data.repository.Repository;
 
 import java.util.Optional;
@@ -10,14 +13,9 @@ import java.util.Optional;
 @Entity
 public class TransactionRecord {
 
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
-
-    // getters and setters omitted for brevity
-}
-
-
 
     private long senderId;
     private long recipientId;

258c2564e1fb5daa430d87148cc6a1cd9bb653c9
phillip feldman <philliplifeldman@gmail.com>
2026-03-12 20:59:44 -0700

    started task 5

---
+++ src/main/java/com/jpmc/midascore/component/BalanceController.java
@@ -0,0 +1,30 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam Long userId) {
+
+        UserRecord user = userRepository.findById(userId).orElse(null);
+
+        float balanceAmount = 0;
+
+        if (user != null) {
+            balanceAmount = user.getBalance();
+        }
+
+        return new Balance(balanceAmount);
+    }
+}
